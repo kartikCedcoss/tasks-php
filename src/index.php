78b31d7c10eb7adeb99cb32bb3abb035bb1b041a
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-24 10:51:39 +0530

    task 1

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+// task 1
+$ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+ 
+
+echo  "The captal of Netherlands is ". $ceu['Netherlands']."<br>"; 
+echo  "The captal of Greece is ". $ceu['Greece']."<br>";
+echo  "The captal of Germany is ". $ceu['Germany'."<br>"];
+?>
+
+
+
+
